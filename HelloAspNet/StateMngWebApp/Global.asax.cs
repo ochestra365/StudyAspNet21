ee8bb4d40909c8d888ffba1c0a1612b73c3b7301
ochestra365 <ochestra365@naver.com>
2021-04-13 09:46:14 +0900

    아침 커밋

---
+++ HelloAspNet/StateMngWebApp/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace StateMngWebApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
+/*세션이 시작되자마자 되는 것*/
