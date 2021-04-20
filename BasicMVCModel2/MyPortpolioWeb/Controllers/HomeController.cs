e23a3fda66c76eee7b0170ff72341e05cae9f5e6
ochestra365 <ochestra365@naver.com>
2021-04-20 12:37:04 +0900

    HTML 공부 필요

--- BasicMVCModel2/MyPortpolioWeb/Controllers/HomeController.cs
+++ BasicMVCModel2/MyPortpolioWeb/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace MyPortpolioWeb.Controllers
         {
             return View();
         }
-
+        public IActionResult Profile()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
