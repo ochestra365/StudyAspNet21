a3241b07379905bd88dccfb3671d07600ebe89ed
ochestra365 <ochestra365@naver.com>
2021-04-21 10:54:03 +0900

    공리를 토대로 이해를 해야 함

---
+++ BasicMVCModel2/MyPortpolioWeb/Controllers/ContactsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyPortpolioWeb.Data;
+using MyPortpolioWeb.Models;
+
+namespace MyPortpolioWeb.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContactsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contacts
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index([Bind()] Contact contact)
+        {
+
+        }
+    }
+}
--- BasicMVCModel2/MyPortpolioWeb/Data/ApplicationDbContext.cs
+++ BasicMVCModel2/MyPortpolioWeb/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MyPortpolioWeb.Models;
 
 namespace MyPortpolioWeb.Data
 {
@@ -12,5 +13,6 @@ namespace MyPortpolioWeb.Data
             : base(options)
         {
         }
+        public DbSet<MyPortpolioWeb.Models.Contact> Contact { get; set; }
     }
 }
