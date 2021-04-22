f1cf8a208de0c73b30a619e2497d8ea8e6398683
ochestra365 <ochestra365@naver.com>
2021-04-22 15:14:34 +0900

    all 커밋

---
+++ BasicMVCModel2/TotalDatabase/dbo/Tables/Boards.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Boards] (
+    [id]        INT            IDENTITY (1, 1) NOT NULL,
+    [Subject]   NCHAR (250)    NOT NULL,
+    [Contents]  NVARCHAR (MAX) NULL,
+    [RegDate]   DATETIME       CONSTRAINT [DF_Boards_RegDate] DEFAULT (getdate()) NULL,
+    [ReadCount] INT            NULL,
+    [Writer]    NVARCHAR (25)  NOT NULL,
+    CONSTRAINT [PK_Boards] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
